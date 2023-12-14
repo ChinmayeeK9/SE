c441865b7ab3e74f6ca4b07bf8cbbad1a21cd163
2020BTEIT00008 <110394857+ChinmayeeK9@users.noreply.github.com>
2023-12-14 19:20:16 +0530

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int add_numbers(int a, int b) {
+    return a - b;  // Bug: Should be addition, but subtraction is used
+}
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    int result = add_numbers(num1, num2);
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}

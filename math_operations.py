17e8dbda5ad84b8dfcdd654496336899a9152867
2020BTEIT00008 <110394857+ChinmayeeK9@users.noreply.github.com>
2023-12-14 20:01:23 +0530

    Update math_operations.py

--- math_operations.py
+++ math_operations.py
@@ -2,10 +2,4 @@
 
 import math
 
-def square_root(number):
-    """Calculate the square root of a number."""
-    return math.sqrt(number)
 
-def power(base, exponent):
-    """Calculate the power of a base raised to an exponent."""
-    return base ** exponent
